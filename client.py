bf01bdf3475722a0ce46bf892b3be477370c77bf
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2023-12-28 15:14:48 -0300

    Complete example

--- client.py
+++ client.py
@@ -8,6 +8,21 @@ sys.path.insert(0, "..")
 
 ADDRESS = "localhost"
 # ADDRESS = "192.168.0.44"
+
+def show_node_info(node, type_node):
+    print("+_+_+_+_+_+_+_+_+_+_+")
+    print(type_node)
+    print("node ",node)
+    browse_name = node.get_browse_name()
+    print("node.get_browse_name: ",browse_name)
+    print("node.get_variables: ",node.get_variables())
+    print("node.get_display_name: ",node.get_display_name())
+    children = node.get_children()
+    print("node.get_children: ",children)
+    print("node.get_parent: ",node.get_parent())
+    methods = node.get_methods()
+    print("node.get_methods: ",methods)
+    print("+_+_+_+_+_+_+_+_+_+_+")
  
 if __name__ == "__main__":
     client = Client(f"opc.tcp://{ADDRESS}:3009")
@@ -23,55 +38,44 @@ if __name__ == "__main__":
         # IMPORTANT
         # Aqui ele retorna os nodos filhos, Root, FoolderType, Server e aqueles criados no servidor
         # Os parametros da funcao filtram os objetos
-        # mas provavelmente terei que separa os objetos do servidor dos objetos criados no servidor (q eu quero)
         references = objectId.get_referenced_nodes(refs=31, direction=BrowseDirection.Both, nodeclassmask=NodeClass.Unspecified, includesubtypes=False)
-
+        names = client.get_namespace_array()
         print("References: ", references)
-        print("===")
-        for node in references:
-            print("+_+_+_+_+_+_+_+_+_+_+")
-            print("node ",node)
-            browse_name = node.get_browse_name()
-            # NamespaceIndex = 0 => Node Padrao do OPC UA
-            # NamespaceIndex != 0 => Node criado no servidor
-            print("node.get_browse_name: ",browse_name)
-            print("node.get_variables: ",node.get_variables())
-            print("node.get_display_name: ",node.get_display_name())
-            print("node.get_children: ",node.get_children())
-            print("node.get_parent: ",node.get_parent())
 
-            # Testar a execucao de um metodo
+        # NamespaceIndex = 0 => Node Padrao do OPC UA
+        # NamespaceIndex != 0 => Node criado no servidor
+        default_nodes = [node for node in references if node.nodeid.NamespaceIndex == 0]
+        custom_nodes = [node for node in references if node.nodeid.NamespaceIndex != 0]
+
+        print("DEFAULT NODES FROM OPC UA")
+        for node in default_nodes:
+            show_node_info(node, "Node")
+
+        print("CUSTOM NODES FROM SERVER")
+        for node in custom_nodes:
+            show_node_info(node, "Node")
+
+            print("++++++Test change var value+++++++")
+            vars = node.get_variables()
+            for var in vars:
+                print(f"{var} - Current Value: ",var.get_value())
+
+            new_value = round(random.random() * 100, 2)
+            print("Vew var value: ", new_value) 
+
             methods = node.get_methods()
-            print("node.get_methods: ",methods)
-            # if len(methods)>0:
-            #     root.call_method(methods[0], 10)
-            print("+++++++++++++")
+            node.call_method(methods[0], new_value)
 
-                    
-        print("===")
+            for var in vars:
+                print(f"{var} - Value After Update: ",var.get_value())
 
-
-
-
-
-
-
-        obj = root.get_child(["0:Objects", "2:Machine1"])
-        temperature = root.get_child(["0:Objects", "2:Machine1", "2:Temperature"])
-        m1Datetime = root.get_child(["0:Objects", "2:Machine1", "2:M1Datetime"])
-        change_temperature = root.get_child(["0:Objects", "2:Machine1", "2:AdjustTemperature"])
-        print("Method: ", change_temperature)
-        print("myobj is: ", obj)
-        print("myData1 is: ", temperature)
-        print("M1Datetime is: ", m1Datetime)
-        while True:
-            print("myData1 = %4.1f" %client.get_node(temperature).get_value())
-            # print("Mais info sobre myData1 = %4.1f" %client.get_node(myData1)...any item that is a variable)
-            # print("myData1 = %4.1f" %client.get_node("ns=2;i=2").get_value())
-            print("m1Datetime = ", client.get_node(m1Datetime).get_value().strftime("%Y-%m-%d     %H:%M:%S"))
-            # print("myDataDatetime = ", client.get_node("ns=2;i=3").get_value().strftime("%Y-%m-%d     %H:%M:%S"))
-            new_value = random.random() * 100
-            # root.call_method(change_temperature, new_value)
-            time.sleep(4)
+            print("+++++++++++++++")
+            
+            print("-------- Show Info about chuildren --------")
+            children = node.get_children()
+            for sub_node in children:
+                show_node_info(sub_node, "SubNode")
+            print("----------------")
+        
     finally:
         client.disconnect()
--- server.py
+++ server.py
@@ -8,14 +8,25 @@ sys.path.insert(0, "..")
 
 ADDRESS = "leonardo.local"
 
+
+def get_set_temperature(variable_to_update):
+    def set_temperature(parent, value):
+        print("New Method")
+        print("Changing temperature to:", value)
+        print("Parent:", parent)
+        variable_to_update.set_value(value)
+        print("++++++++")
+        return [ua.Variant(value, ua.VariantType.Float)]
+
+    return set_temperature
+
+
 def adjust_temperature(parent, value):
     print("Changing temperature to:", value)
     print("Parent:", parent)
-    # global myData1  # Referenciar a variável global
-    # myData1.set_value(value) 
     return [ua.Variant(value, ua.VariantType.Float)]
  
-if __name__ == "__main__":
+
     # server = Server()
     # server.set_endpoint(f"opc.tcp://{ADDRESS}:3009")
     
@@ -47,46 +58,56 @@ if __name__ == "__main__":
     #     #close connection, remove subcsriptions, etc
     #     server.stop()
 
-    server = Server()
-    server.set_endpoint(f"opc.tcp://{ADDRESS}:3009")
-    # setup our own namespace, not really necessary but should as spec
-    uri = "Catraport OPC-UA Server"
-    idx = server.register_namespace(uri)
-    print("IDX: ", idx)
-    objects = server.get_objects_node()
+server = Server()
+server.set_endpoint(f"opc.tcp://{ADDRESS}:3009")
+server.set_server_name("Test OPC UA Server")
+enums = server.load_enums()
+# setup our own namespace, not really necessary but should as spec
+uri1 = "Leonardo"
+uri2 = "Maju"
 
-    #+++++++++++++++++++++++++++++++++
-    machine5Obj = objects.add_object(idx, "5. MACHINE_STAMPING")
-    sensor_value = random.randint(0, 100)/10
-    sensor_value_var5 = machine5Obj.add_variable(idx, "Temperature", sensor_value)
-    machine_last_update_var = machine5Obj.add_variable(idx, "LastUpdate", datetime.now())
+idx1 = server.register_namespace(uri1)
+idx2 = server.register_namespace(uri2)
+print("IDX1: ", idx1)
+print("IDX2: ", idx2)
+objects = server.get_objects_node()
+#+++++++++++++++++++++++++++++++++
+machine5Obj = objects.add_object(idx1, "5. MACHINE_STAMPING")
+machine5Obj.add_object(idx1, "Sensor Temperature")
+sensor_value = random.randint(0, 100)/10
+sensor_value_var5 = machine5Obj.add_variable(idx1, "Temperature", sensor_value)
+machine_last_update_var1 = machine5Obj.add_variable(idx1, "LastUpdate", datetime.now())
 
-    machine_last_update_var.set_writable()
-    sensor_value_var5.set_writable()
-    method = machine5Obj.add_method(idx, "AdjustTemperature", adjust_temperature, [ua.VariantType.Float], [ua.VariantType.Float])
-    #+++++++++++++++++++++++++++++++++
-    machine4Obj = objects.add_object(idx, "4. MACHINE_STAMPING")
-    sensor_value = random.randint(0, 100)/10
-    sensor_value_var4 = machine4Obj.add_variable(idx, "Temperature", sensor_value)
-    machine_last_update_var = machine4Obj.add_variable(idx, "LastUpdate", datetime.now())
+sensor_value_var5.set_writable()
+machine_last_update_var1.set_writable()
+method = machine5Obj.add_method(idx1, "AdjustTemperature", get_set_temperature(sensor_value_var5), [ua.VariantType.Float], [ua.VariantType.Float])
+#+++++++++++++++++++++++++++++++++
+machine4Obj = objects.add_object(idx2, "4. MACHINE_STAMPING")
+machine4Obj.add_object(idx2, "Sensor Temperature")
+sensor_value = random.randint(0, 100)/10
+sensor_value_var4 = machine4Obj.add_variable(idx2, "Temperature", sensor_value)
+machine_last_update_var2 = machine4Obj.add_variable(idx2, "LastUpdate", datetime.now())
 
-    machine_last_update_var.set_writable()
-    sensor_value_var4.set_writable()
-    method = machine4Obj.add_method(idx, "AdjustTemperature", adjust_temperature, [ua.VariantType.Float], [ua.VariantType.Float])
-    #+++++++++++++++++++++++++++++++++
+sensor_value_var4.set_writable()
+machine_last_update_var2.set_writable()
+method = machine4Obj.add_method(idx2, "AdjustTemperature", get_set_temperature(sensor_value_var4), [ua.VariantType.Float], [ua.VariantType.Float])
+#+++++++++++++++++++++++++++++++++
 
 
-    server.start()
-    try:
-        count = 0
-        while True:
-            time.sleep(2)
-            # myDataDatetime.set_value(datetime.datetime.now())
-            sensor_value = random.randint(0, 100)/10
-            sensor_value_var5.set_value(sensor_value)
+server.start()
+try:
+    count = 0
+    while True:
+        time.sleep(2)
+        machine_last_update_var1.set_value(datetime.datetime.now())
+        machine_last_update_var2.set_value(datetime.datetime.now())
 
-            sensor_value = random.randint(0, 100)/10
-            sensor_value_var4.set_value(sensor_value)
-    finally:
-        #close connection, remove subcsriptions, etc
-        server.stop()
+        # Remove comments to update the value of the variables on server
+        sensor_value = random.randint(0, 100)/10
+        # sensor_value_var5.set_value(sensor_value)
+
+        sensor_value = random.randint(0, 100)/10
+        # sensor_value_var4.set_value(sensor_value)
+finally:
+    #close connection, remove subcsriptions, etc
+    server.stop()
